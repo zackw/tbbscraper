18600932da514143fb7ba230eb93caa56585feee
Zack Weinberg <zackw@panix.com>
2014-02-22 13:05:34 -0500

    Update alexa source for new DB schema.
    Also, better normalization and don't add 'www.' where it doesn't make sense.

--- sources/alexa.py
+++ sources/alexa.py
@@ -16,7 +16,7 @@ import urllib2
 import urlparse
 import zipfile
 
-from . import urldb
+import urldb
 
 def parse_args():
     ap = argparse.ArgumentParser(description="Download the current Alexa "
@@ -63,6 +63,89 @@ def download_sitelist(args, datestamp):
     os.rename(cached_csv+".tmp", cached_csv)
     return cached_csv
 
+def normalize_url(spliturl):
+    """Given a SplitResult object returned from urlparse.urlsplit,
+       normalize its network location and path.  This means stripping
+       empty username and password fields, stripping a port number
+       that is redundant to the scheme, and converting an empty path
+       to a single slash. Returns a revised SplitResult."""
+
+    # Don't mess with schemes that don't use a netloc.
+    if spliturl.netloc == "":
+        return spliturl
+
+    path = spliturl.path
+    host = spliturl.hostname
+    port = spliturl.port
+    user = spliturl.username
+    pwrd = spliturl.password
+
+    if host is None:
+        raise ValueError("url with no host: " + spliturl.geturl())
+
+    if port is None:
+        port = ""
+    else:
+        if ((spliturl.scheme == "http" and port == 80) or
+            (spliturl.scheme == "https" and port == 443)):
+            port = ""
+        else:
+            port = ":{}".format(port)
+
+    if user is None:
+        user = ""
+    if pwrd is None:
+        pwrd = ""
+
+    # We don't have to worry about ':' or '@' in the user and password
+    # strings, because urlparse does not do %-decoding on them.
+    if user == "" and pwrd == "":
+        auth = ""
+    elif pwrd == "":
+        auth = "{}@".format(user)
+    else:
+        auth = "{}:{}@".format(user, pwrd)
+
+    netloc = auth + host + port
+
+    if path == '':
+        path = '/'
+
+    return urlparse.SplitResult(spliturl.scheme,
+                                netloc,
+                                path,
+                                spliturl.query,
+                                spliturl.fragment)
+
+def to_https(spliturl):
+    return urlparse.SplitResult("https",
+                                spliturl.netloc,
+                                spliturl.path,
+                                spliturl.query,
+                                spliturl.fragment)
+
+def to_siteroot(spliturl):
+    return urlparse.SplitResult(spliturl.scheme,
+                                spliturl.netloc,
+                                "/",
+                                spliturl.query,
+                                spliturl.fragment)
+
+def add_www(spliturl):
+    if "@" in spliturl.netloc:
+        (auth, rest) = spliturl.netloc.split("@", 1)
+        netloc = auth + "@www." + rest
+    else:
+        netloc = "www." + spliturl.netloc
+
+    return urlparse.SplitResult(spliturl.scheme,
+                                netloc,
+                                spliturl.path,
+                                spliturl.query,
+                                spliturl.fragment)
+
+no_www_re = re.compile(r"^(?:\d+\.\d+\.\d+\.\d+$|\[[\dA-Fa-f:]+\]$|www\.)")
+
 def add_urls_from_site(cur, site, ordinal, oid, already_seen):
     # Subroutine of process_sitelist.
     #
@@ -94,22 +177,67 @@ def add_urls_from_site(cur, site, ordinal, oid, already_seen):
     # Because the database schema requires the ordinal+oid to be unique,
     # we shift the ordinal left three bits to make room for a prefix index
     # and an indication of whether or not there was a path component.
+    #
+    # It does not make sense to prepend 'www.' if 'site' already starts with
+    # 'www.' or if it is an IP address.
+
+    parsed = normalize_url(urlparse.urlsplit("http://" + site))
+
+    assert parsed.path != ""
+    if parsed.path != "/":
+        root = to_siteroot(parsed)
+        need_path = True
+    else:
+        root = parsed
+        need_path = False
+
+    urls = [ (0, root.geturl()),
+             (1, to_https(root).geturl()) ]
+
+    host = root.hostname
+    if no_www_re.match(host):
+        need_www = False
+    else:
+        need_www = True
+        with_www = add_www(root)
+        urls.extend([ (2, with_www.geturl()),
+                      (3, to_https(with_www).geturl()) ])
+
+
+    if need_path:
+        urls.extend([ (4, parsed.geturl()),
+                      (5, to_https(parsed).geturl()) ])
+
+        if need_www:
+            with_www = add_www(parsed)
+            urls.extend([ (6, with_www.geturl()),
+                          (7, to_https(with_www).geturl()) ])
 
     ordinal = int(ordinal) * 8
 
-    if "/" in site:
-        pathbit = 4
-    else:
-        pathbit = 0
-        site = site + "/"
+    for tag, url in urls:
+        if url in already_seen:
+            continue
+        already_seen.add(url)
 
-    for i, prefix in enumerate(("http://", "https://",
-                                "http://www.", "https://www.")):
-        url = urlparse.urlparse(prefix + site).geturl()
-        if url not in already_seen:
-            cur.execute("INSERT INTO urls VALUES(?, ?, ?)",
-                        (oid, ordinal + pathbit + i, url))
-            already_seen.add(url)
+        # It is a damned shame that there is no way to do this
+        # in one SQL operation.
+        cur.execute("SELECT id FROM url_strings WHERE url = ?",
+                    (url,))
+        row = cur.fetchone()
+        if row is not None:
+            uid = row[0]
+        else:
+            cur.execute("INSERT INTO url_strings VALUES(NULL, ?)",
+                        (url,))
+            uid = cur.lastrowid
+
+        # We want to add an url-table entry for this URL even if it's
+        # already there from some other source; we only drop them if
+        # they are redundant within this data set.
+        cur.execute("INSERT INTO urls VALUES(?, ?, ?)",
+                    (oid, ordinal + tag, uid))
+
 
 def process_sitelist(db, sitelist_name, datestamp):
 
@@ -125,21 +253,21 @@ def process_sitelist(db, sitelist_name, datestamp):
         # captured by the source name (with its embedded date stamp)
         # and the ordinal within Alexa's list, which is what we use
         # for the origin_id.
-        cur.execute("INSERT INTO origins VALUES(NULL, ?)",
-                    ("alexa_" + datestamp,))
-        oid = cur.lastrowid
-
-        db.commit()
+        label = ("alexa_" + datestamp,)
+        cur.execute("SELECT id FROM origins WHERE label = ?", label)
+        row = cur.fetchone()
+        if row is not None:
+            oid = row[0]
+        else:
+            cur.execute("INSERT INTO origins VALUES(NULL, ?)", label)
+            oid = cur.lastrowid
+            db.commit()
 
         with gzip.GzipFile(sitelist_name, "r") as sitelist:
             for i, line in enumerate(sitelist):
                 ordinal, _, site = line.partition(",")
                 site = site.rstrip()
                 add_urls_from_site(cur, site, ordinal, oid, already_seen)
-                x = site.find("/")
-                if x != -1:
-                    add_urls_from_site(cur, site[:x], ordinal, oid,
-                                       already_seen)
                 if i % 1000 == 0:
                     db.commit()
                     sys.stderr.write("\r{}".format(i))
