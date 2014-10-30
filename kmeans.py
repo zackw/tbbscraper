c500007fb8118b799e4b88d2ad6795be1e8217bb
OMGitsHongyu <hongyuz@andrew.cmu.edu>
2014-10-30 00:35:44 +0000

    possible unusable on code feature

--- kmeans.py
+++ kmeans.py
@@ -1,23 +1,26 @@
 import psycopg2
 import time
 import os
+import numpy as np
+
+from kmeanspp import *
 # from sparse import SparseList
 
-# def getBinaryFeatureMap(db,rowName):
-	# featureMap = {}
-	# cursor = db.cursor()
-	# cursor.execute('select distinct ' + rowName + ' from features_test')
-	# values = cursor.fetchall()
-	# counter = 0
-	# for value in values:
-		# if(value not in featureMap):
-			# featureMap[value] = counter
-			# counter += 1
-		# else:
-			# print('Error distinct had the same value twice: ' + str(value))
-	# cursor.close()
-	# print(rowName + ' has ' + str(len(featureMap)) + ' distinct values')
-	# return featureMap
+def getBinaryFeatureMap(db,rowName):
+	featureMap = {}
+	cursor = db.cursor()
+	cursor.execute('select distinct ' + rowName + ' from features_test')
+	values = cursor.fetchall()
+	counter = 0
+	for value in values:
+		if(value not in featureMap):
+			featureMap[value] = counter
+			counter += 1
+		else:
+			print('Error distinct had the same value twice: ' + str(value))
+	cursor.close()
+	print(rowName + ' has ' + str(len(featureMap)) + ' distinct values')
+	return featureMap
 
 # def getSparseList(value,featureMap):
 	# sparseList = SparseList()
@@ -31,7 +34,7 @@ start_time = time.time()
 query = '''
 	select locale, url, code, detail, isRedir, redirDomain, 
 	html_length, content_length, dom_depth, number_of_tags, unique_tags, 
-	tfidf from features_test limit 5
+	tfidf from features_test limit 2000
 	'''
 
 scheme = "dbname=ts_analysis"
@@ -40,7 +43,7 @@ scheme = "dbname=ts_analysis"
 # pages = []
 db = psycopg2.connect(scheme)
 
-# codeFeatureMap = getBinaryFeatureMap(db,'code')
+#codeFeatureMap = getBinaryFeatureMap(db,'code')
 # detailFeatureMap = getBinaryFeatureMap(db,'detail')
 # redirDomainFeatureMap = getBinaryFeatureMap(db,'redirDomain')
 
@@ -48,7 +51,7 @@ db = psycopg2.connect(scheme)
 # cursor.itersize = 100
 # cursor.execute(query)
 # row = cursor.fetchone()
-savedpage = []
+#savedpage = []
 counter  = 0
 with db, \
     db.cursor("pagedb_qtmp_{}".format(os.getpid())) as cur:
@@ -67,19 +70,28 @@ with db, \
         # Adding code features
         # code = getSparseList(row[2],codeFeatureMap)
         #print(page[:11])
-        print(sum(tfidf))
-        print(len(tfidf))
+        print(len(page))
+       # print(page[0:11])
+        if counter == 1:
+            savedpage = page[12:60]
+        else:
+            savedpage = np.row_stack((savedpage,page[12:60]))
         # pages.append(page)
         # row = cursor.fetchone()))
-        savedpage.extend(page)
+        #savedpage.extend(page)
+print(savedpage.shape)
 
 #cursor.close()
 db.close()
-
+kmpp = KMeansPlusPlus(savedpage, 8 ,max_iterations=10)
+kmpp.cluster()
+cls = kmpp.clusters
+print(cls)
+"""
 print(len(savedpage))
 thefile = open("testlist", "w")
 for item in savedpage:
     thefile.write("%s\n" % item)
 thefile.close()
-
+"""
 print("--- " + str(time.time() - start_time) + " seconds ---")
