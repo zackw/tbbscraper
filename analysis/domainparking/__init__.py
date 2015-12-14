cf7b8c0aad778c255ec99fe1e657c5eb1762a2c0
Zack Weinberg <zackw@panix.com>
2015-12-14 12:18:03 -0500

    Simplify domain parking tester and make the way it works less hardcoded.

--- analysis/domainparking/__init__.py
+++ analysis/domainparking/__init__.py
@@ -1,239 +1,164 @@
-import re
-from urllib.parse import urlparse
-from publicsuffix import PublicSuffixList
+#! /usr/bin/python3
 
-class DomainParking:
-    '''
-        This class can be used to classify webpages as parked or not.
+import collections
+import configparser
+import os
+import regex as re
 
-        Properties:
-            type                - Determines the set of rules to use from all
-                                  available rules
-            size_limit          - if the content of a pages is smaller than
-                                  size_limit, then it is classified
-                                  automatically as "not parked"
-            rulesToUse          - based on "type" this is the list of rules
-                                  that can be used for parked classification
-            parkedRuleFile      - File of rules that are matching a given
-                                  specific parked page
-            weakparkedRule1File - Rules that are matching general domain
-                                  parking terms
-            weakparkedRule2File - Rules that are matching general domain
-                                  parking terms
-    '''
+__all__ = ('ParkingClassifier', 'ParkingClassification')
 
-    def __init__(self, type = 'full', size_limit = 200000):
-        '''
-            Parameters:
-                type        - full: using all regexes
-                            - balanced: using regexes that were useful
-                              at least once (precisely: tp > fp for this rule)
-                            - min: using the minimal set of regexes that cover
-                              parked domains found by using "full" <- it might
-                              not find about 3% of pared pages
-                size_limit  - if the content of a pages is smaller than
-                              size_limit, then it is classified automatically
-                              as "not parked"
-        '''
-        self.type = type
-        self.size_limit = size_limit
-        self.rulesToUse = None
-        if type in ['full','balanced','min']:
-            self.parkedRuleFile = 'parkedRules/reExpressions.in'
-            self.weakparkedRule1File = 'parkedRules/reExpressions-weak1.in'
-            self.weakparkedRule2File = 'parkedRules/reExpressions-weak2.in'
-        else:
-            self.parkedRuleFile = ''
-            self.weakparkedRule1File = ''
-            self.weakparkedRule2File = ''
+MODE_FILE = os.path.join(os.path.dirname(__file__), "modes.cf")
+RULE_FILE = os.path.join(os.path.dirname(__file__), "rules.cf")
 
-        if type in ['balanced','min']:
-            f = open('parkedRules/'+type+'_rules', mode = 'r')
-            self.rulesToUse = f.read().split('\n')
-            f.close()
+class Ruleset:
+    """A set of tagged regular expressions."""
+    def __init__(self, label, ruledict, only=None):
+        self.label = label
+        self.rules = [
+            (tag, re.compile(rule, re.VERBOSE|re.IGNORECASE))
+            for tag, rule in ruledict.items()
+            if (only is None or tag in only)
+        ]
 
-        self.psl = None
+    def test(self, text):
+        """Match TEXT against all of the regular expressions in this
+           set, and return the tags of those that matched."""
+        return [
+            tag for tag, rule in self.rules
+            if rule.search(text)
+        ]
 
-    def changeRule(self,parkedRuleFile,weakparkedRule1File,weakparkedRule2File):
-        '''
-        Function changeRule can be used to set custom rule files.
+ParkingClassification = collections.namedtuple(
+    "ParkingClassification",
+    ("is_parked", "rules_matched"))
 
-        Parameters:
-            parkedRuleFile      - File of rules that are matching a given
-                                  specific parked page
-            weakparkedRule1File - Rules that are matching general domain
-                                  parking terms
-            weakparkedRule2File - Rules that are matching general domain
-                                  parking terms
-        '''
-        self.parkedRuleFile = parkedRuleFile
-        self.weakparkedRule1File = weakparkedRule1File
-        self.weakparkedRule2File = weakparkedRule2File
+class ParkingClassifier:
+    """Classifier which decides whether a webpage is a domain-parking
+       site.
 
-    def changSizeLimit(self,size_limit):
-        '''
-        Change Size limit
+       Methods:
+           isParked(html)  - returns a named 2-tuple:
+                             (is_parked, rules_matched)
+                             is_parked is true or false, and rules is
+                             the list of all rules that matched.
 
-        Parameters:
-            size_limit    - the nez size limit
-        '''
+       Properties:
+           mode            - The classification mode (see modes.cf)
+           size_limit      - Pages larger than this are assumed not
+                             to be parked.
+    """
+
+    def __init__(self, *,
+                 mode='full',
+                 size_limit=200000,
+                 modefile=MODE_FILE,
+                 rulefile=RULE_FILE):
+
+        self.mode       = mode
         self.size_limit = size_limit
 
-    def __getRules__(self,fileName):
-        '''
-        Gets the rules from a file and returns it as a list.
+        mode_p = configparser.ConfigParser(interpolation=None,
+                                           allow_no_value=True)
+        with open(modefile) as m:
+            mode_p.read_file(m, modefile)
 
-        Parameters:
-        fileName    - the filename of the rules we are going to load
-        '''
-        rules = []
-        f = open(fileName, mode='r')
-        for line in f:
-            exp = line[:-1].split(" ")
-            if ((self.rulesToUse is None) or
-                (fileName in [self.weakparkedRule1File,
-                              self.weakparkedRule2File]) or
-                (exp[0] in self.rulesToUse)):
-                rule = []
-                rule.append(exp[0])
-                rule.append(re.compile(exp[1].strip(), re.IGNORECASE))
-                rules.append(rule)
-        f.close()
-        return rules
-
-    def loadRules(self):
-        '''
-        Calling this function we will load the rules used for detecting
-        parked domains.
-        '''
-        if ((self.parkedRuleFile != '') and
-            (self.weakparkedRule1File != '') and
-            (self.weakparkedRule2File != '')):
-            self.rules = self.__getRules__(self.parkedRuleFile)
-            self.weak_rules1 = self.__getRules__(self.weakparkedRule1File)
-            self.weak_rules2 = self.__getRules__(self.weakparkedRule2File)
-
-    def isParkedDomain(self, dom, domain):
-        '''
-        This function tests whether a page is parked or not. It
-        returns a "result" dict which includes "parked" or "notparked"
-        and it also returns the matched rules.
-
-        Parameters:
-            don    - the raw html of the page
-            domain - The registered domain name of the webpage where
-                     we get the content from (example.co.uk from
-                     www.example.co.uk)
-        '''
-        result = {}
-        result['isParked'] = ''
-        result['rules'] = []
-        if len(dom) <= self.size_limit:
-            parked_rules = self.__applyRules__(dom,self.rules)
-            weak_parked_rules1 = self.__applyRules__(dom,self.weak_rules1)
-            weak_parked_rules2 = self.__applyRules__(dom,self.weak_rules2)
-            '''A page is classified as parked if a rule is found from
-               "parked_rules" or if a rule is found from both
-               "weak_parked_rules1" and "weak_parked_rules2".
-
-            '''
-            if ((len(parked_rules) > 0) or
-                ((len(weak_parked_rules1) > 0) and
-                 (len(weak_parked_rules2) > 0))):
-                result['isParked'] = 'parked'
-                for rule in parked_rules:
-                    result['rules'].append(rule)
-                for rule in weak_parked_rules1:
-                    result['rules'].append(rule)
-                for rule in weak_parked_rules2:
-                    result['rules'].append(rule)
-            else:
-                result['isParked'] = 'notparked'
+        if mode == 'full':
+            only = None
+        elif mode in mode_p:
+            only = frozenset(mode_p[mode].keys())
         else:
-            result['isParked'] = 'notparked'
-        '''
-            This is a special rule which needs the input parameter "domain"
-        '''
-        if result['isParked'] == 'notparked':
-            for keyword in ['tppunknown.com',
-                            '.'.join(domain.split('.')[-2:])]:
-                general_parking1 = 'Click here to go to ' + keyword
-                general_parking2 = '<meta name="keywords" content="'+keyword+'">'
-                if ((general_parking1.lower() in dom.lower()) and
-                    (general_parking2.lower() in dom.lower())):
-                    result['isParked'] = 'parked'
-                    result['rules'].append('generalparking')
+            raise ValueError("no mode definition for {!r} in {!r}"
+                             .format(mode, modefile))
 
-        return result
+        rule_p = configparser.ConfigParser(interpolation=None)
+        with open(rulefile) as r:
+            rule_p.read_file(r, rulefile)
 
-    def isParkedUrl(self, dom, url):
-        '''
-        Same as isParkedDomain but takes a full URL instead of a registered
-        domain name.
-        '''
-        if self.psl is None:
-            self.psl = PublicSuffixList()
+        for ruleset in ("strong", "weak1", "weak2"):
+            if ruleset not in rule_p:
+                raise ValueError("ruleset {!r} missing from {!r}"
+                                 .format(ruleset, rulefile))
 
-        # This function actually returns the "registered domain", i.e.
-        # the public suffix plus one more label to the left.
-        domain = psl.get_public_suffix(urlparse(url.strip()).hostname)
+        self.strong_rules = Ruleset("strong", rule_p["strong"], only)
+        self.weak_rules_1 = Ruleset("weak1",  rule_p["weak1"],  only)
+        self.weak_rules_2 = Ruleset("weak2",  rule_p["weak2"],  only)
 
-        return self.isParkedDomain(dom, domain)
+    def isParked(self, html):
+        """Test whether HTML appears to be a webpage from a parked domain.
+           Returns a 2-tuple (is_parked, rules_matched) where is_parked
+           is a boolean and rules_matched is the list of all rules
+           that matched.
 
-    def isParked(self, dom, domain=None, url=None):
-        '''
-        Generic entry point can be used with either domain= or url= arguments.
-        '''
-        if domain:
-            if url:
-                raise ValueError("need either domain= or url=, but not both")
-            return self.isParkedDomain(dom, domain)
-        elif url:
-            return self.isParkedUrl(dom, url)
-        else:
-            raise ValueError("need either domain= or url= argument")
+           A page is considered to be parked if it matches at least
+           one of the "strong" rules, or if it matches at least one of
+           the "weak1" rules _and_ at least one of the "weak2" rules.
+        """
+
+        m_strong = self.strong_rules.test(html)
+        m_weak1  = self.weak_rules_1.test(html)
+        m_weak2  = self.weak_rules_2.test(html)
+
+        is_parked = bool(m_strong) or (bool(m_weak1) and bool(m_weak2))
+        rules_matched = m_strong + m_weak1 + m_weak2
+        rules_matched.sort()
+        return ParkingClassification(is_parked, rules_matched)
 
 #
 # Self-tests
 #
 
-def testParkedSample(content_dir, filename, domainParking):
-        ok = 0
-        errors = []
-        with open(filename) as f:
-            for line in f:
-                data = line[:-1].split(',')
-                cls = data[6]
-                url = data[5]
-                id = data[0]
-                content_file = content_dir + '/' + id + '.html'
-                with open(content_file, encoding='utf-8') as cf:
-                    content = cf.read()
+def testParkedSample(content_dir, filename, classifier, outf):
+    import time, datetime
 
-                result = domainParking.isParkedUrl(content, url)
-                if(result['isParked'] == cls):
-                    ok += 1
-                else:
-                    errors.append(id)
+    ok = 0
+    errors = []
+    start = time.monotonic()
 
-        print(filename)
-        print('Ok: ' + str(ok))
-        print('Errors: ' + " ".join(sorted(errors)))
-        print()
+    with open(filename) as f:
+        for line in f:
+            data = line[:-1].split(',')
+            id = data[0]
+            if data[6] == 'parked': cls = True
+            elif data[6] == 'notparked': cls = False
+            else:
+                errors.append("{}: {!r} is not 'parked' or 'notparked'"
+                              .format(id, data[6]))
+                continue
 
-def testRules(type):
-    import time
+            content_file = content_dir + '/' + id + '.html'
+            with open(content_file, encoding='utf-8') as cf:
+                content = cf.read()
 
-    domainParking = DomainParking(type)
-    domainParking.loadRules()
+            result = classifier.isParked(content)
+            if(result.is_parked == cls):
+                ok += 1
+            else:
+                errors.append("{}: exp {} got {} rm {}"
+                              .format(id, cls, result.is_parked,
+                                      result.rules_matched))
 
-    start_time = time.monotonic()
-    testParkedSample('parkedRESamples', 'parkedRE_samples-200.txt',
-                     domainParking)
-    testParkedSample('parkedRESamples2', 'parkedRE_samples2-200.txt',
-                     domainParking)
-    print("--- " + str(time.monotonic() - start_time) + " seconds ---")
+    interval = datetime.timedelta(seconds=time.monotonic() - start)
+
+    errors.sort()
+    outf.write("{} (mode {!r}): {}\n"
+               "OK: {}\n"
+               "Errors:\n  {}\n\n"
+               .format(filename, classifier.mode, interval, ok,
+                       "\n   ".join(errors)))
+
+    return (not errors)
+
+def testRules(mode, outf, samples):
+    classifier = ParkingClassifier(mode=mode)
+    success = True
+    for i in range(0, len(samples), 2):
+        success = testParkedSample(
+            samples[i], samples[i+1], classifier, outf) and success
+    return success
 
 if __name__ == '__main__':
-    for type in ('full', 'balanced', 'min'):
-        testRules(type)
+    import sys
+    success = True
+    for mode in ('full', 'balanced', 'min'):
+        success = testRules(mode, sys.stdout, sys.argv[1:]) and success
+    sys.exit(0 if success else 1)
