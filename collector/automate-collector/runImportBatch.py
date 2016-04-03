db219959d5675a81c6eb95af829fa66391ca3a04
Srujana Peddada <srujana.peddada@gmail.com>
2016-04-03 17:20:12 -0400

    Fixed some bugs in runImportBatch

--- collector/automate-collector/runImportBatch.py
+++ collector/automate-collector/runImportBatch.py
@@ -2,22 +2,23 @@
 
 import traceback
 import os
+import sys
 from subprocess import check_call
 
 
-def runImportBatch (dbname, dirs)
+def runImportBatch (dbname, dirs):
 
     #TODO: better exception handling
     try:
         cmd = ["python", os.path.dirname(__file__)+"/../scripts/import_batch", dbname, dirs]
         check_call (cmd)
-    except Exception.
+    except Exception:
         print "Error running import_batch"
         traceback.print_exc()
 
 def main ():
     dbname = sys.argv[1]
-    dirs = sys.argv[2:]
+    dirs = sys.argv[2]
     print ("main")
     runImportBatch(dbname, dirs)
     print "runImportBatch Done"
