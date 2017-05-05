081e2dca72b832f96afbe3be6492ec00a72f3fde
Zack Weinberg <zackw@panix.com>
2017-05-05 09:22:50 -0400

    neterr-details: don't use libcurl

--- collector/scripts/neterr-details.c
+++ collector/scripts/neterr-details.c
@@ -6,14 +6,18 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * There is NO WARRANTY.
+ * Partially based on public-domain example code from the gnutls manual:
+ * https://gnutls.org/manual/
  *
- *     neterr-details URL
+ *     neterr-details HOST PORT
  *
- * attempts to connect to the server for URL (using curl) and reports
- * details of any network error that may happen.  If the connection and TLS
- * handshake (when applicable) are successful, it immediately disconnects
- * again and reports "success" -- no application-layer payload is ever
- * transmitted.
+ * attempts to connect to HOST at PORT, and reports details of any
+ * network error that may happen.  PORT must be numeric.  If PORT is
+ * on a hardwired list of TLS-native TCP ports, and TCP connection is
+ * successful, also performs a TLS handshake.  If connection and TLS
+ * handshake (when applicable) are successful, it immediately
+ * disconnects again and reports "success" -- no application-layer
+ * payload is ever transmitted.
  *
  * Output is to stdout and consists of one or more key:value\n strings.
  * The first of these will always be "status:STATUS\n" where STATUS is
@@ -43,301 +47,296 @@
  * CERTIFICATE is the certificate in raw base64 format (no ------BEGIN/END
  * CERTIFICATE------- and no internal newlines).
  *
- * Will only exit unsuccessfully when initializing curl fails, or in case
- * of an internal-to-curl failure, in which case there will be an error
- * message on stderr and nothing on stdout.
- *
- * It is assumed that you do not need to set CURLOPT_CAINFO nor CURLOPT_CAPATH
- * for certificate validation to work.
+ * Will only exit unsuccessfully upon an internal failure within
+ * gnutls, in which case there will be an error message on stderr and
+ * nothing on stdout.
  */
 
+#define _GNU_SOURCE 1
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define CURL_NO_OLDIES 1
-#include <curl/curl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <unistd.h>
 
-#ifndef CURL_GLOBAL_ACK_EINTR
-#define CURL_GLOBAL_ACK_EINTR 0
+#include <gnutls/gnutls.h>
+#include <gnutls/x509.h>
+
+#if defined __GNUC__ && __GNUC__ >= 3
+#define UNUSED(param) param __attribute__((unused))
+#define NORETURN void __attribute__((noreturn))
+#else
+#define UNUSED(param) param
+#define NORETURN void
 #endif
 
-static void
-dump_errbuf(CURLcode res, const char *errbuf, FILE *stream)
+static NORETURN
+fatal_perror(const char *msg)
 {
-  size_t len = strlen(errbuf);
-  if (len > 0) {
-    if (errbuf[len-1] == '\n')
-      ((char *)errbuf)[len-1] = '\0';
-    fputs(errbuf, stream);
-  } else {
-    fputs(curl_easy_strerror(res), stream);
-  }
-  fprintf(stream, " (%d)\n", res);
+  perror(msg);
+  exit(1);
 }
 
-static void
-detail_dump_errbuf(CURLcode res, const char *errbuf)
+static struct addrinfo *
+dns_lookup(const char *host, const char *port)
 {
-  fputs("detail:", stdout);
-  dump_errbuf(res, errbuf, stdout);
-}
+  struct addrinfo *res = 0;
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_flags    = AI_V4MAPPED|AI_ADDRCONFIG|AI_NUMERICSERV;
+  hints.ai_family   = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
 
-static void
-cert_dump_one(const char *cert)
-{
-  fputs("cert:", stdout);
-  const char *p, *q;
-  /* The first line is always "Cert:-----BEGIN CERTIFICATE-----\n".
-     Skip this.  */
-  p = strchr(cert, '\n');
-  if (!p) {
-    puts("<malformed>");
-    return;
-  }
-  p++;
-  while (*p && strcmp(p, "-----END CERTIFICATE-----\n")) {
-    q = strchr(p, '\n');
-    if (!q) {
-      puts("<unexpected EOF>");
-      return;
+  int err = getaddrinfo(host, port, &hints, &res);
+  if (err) {
+    /* The getaddrinfo-level behavior upon NXDOMAIN can be any of these.  */
+    if (err == EAI_ADDRFAMILY || err == EAI_NODATA || err == EAI_NONAME) {
+      puts("status:dns-notfound");
+    } else {
+      puts("status:dns-error");
+      if (err == EAI_SYSTEM) {
+        printf("detail:%s\n", strerror(errno));
+      } else if (err != 0) {
+        printf("detail:%s\n", gai_strerror(err));
+      }
     }
-    fwrite(p, 1, q - p, stdout);
-    p = q + 1;
+    if (res) {
+      freeaddrinfo(res);
+    }
+    return 0;
   }
-  putchar('\n');
+  else if (!res) {
+    /* This can *also* happen for an NXDOMAIN.  */
+    puts("status:dns-notfound");
+    return 0;
+  }
+
+  return res;
 }
 
+static volatile sig_atomic_t timeout_expired;
 static void
-cert_dump_chain(CURL *curl, const char *errbuf)
+timeout_handler(int UNUSED(n))
 {
-  struct curl_certinfo *chain = 0;
-  CURLcode res;
-  if ((res = curl_easy_getinfo(curl, CURLINFO_CERTINFO, &chain)) != 0) {
-    fputs("error: getinfo(CERTINFO): ", stderr);
-    dump_errbuf(res, errbuf, stderr);
-    exit(1);
-  }
-  if (chain && chain->num_of_certs) {
-    for (int i = 0; i < chain->num_of_certs; i++) {
-      struct curl_slist *slist;
-      for (slist = chain->certinfo[i]; slist; slist = slist->next)
-        if (!strncmp(slist->data, "Cert:", sizeof "Cert:" - 1))
-          cert_dump_one(slist->data);
-    }
-  }
+  timeout_expired = 1;
 }
 
 static int
-status_dns_error(CURLcode res, const char *errbuf)
+tcp_connect(struct addrinfo *addrlist)
 {
-  puts("status:dns-error");
-  return 1;
-}
+  struct addrinfo *ai;
+  int sock, err = 0, rv;
 
-static int
-status_tcp_error(CURL *curl, const char *errbuf)
-{
-  CURLcode res;
-  long err;
-  if ((res = curl_easy_getinfo(curl, CURLINFO_OS_ERRNO, &err)) != 0) {
-    fputs("error: getinfo(OS_ERRNO): ", stderr);
-    dump_errbuf(res, errbuf, stderr);
-    exit(1);
+  /* The easiest way to implement a connection timeout is with
+     good old-fashioned alarm(2).  */
+  struct sigaction sa, oALRM, oINT;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = timeout_handler;
+  sa.sa_flags   = 0; /* DO interrupt blocking system calls! */
+  if (sigaction(SIGALRM, &sa, &oALRM) || sigaction(SIGINT, &sa, &oINT))
+    fatal_perror("sigaction");
+
+  for (ai = addrlist; ai; ai = ai->ai_next) {
+    sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sock == -1) {
+      err = errno;
+      continue;
+    }
+
+    timeout_expired = 0;
+    alarm(60);
+    do {
+      rv = connect(sock, ai->ai_addr, ai->ai_addrlen);
+    } while (rv == -1 && errno == EINTR && !timeout_expired);
+    if (rv == 0)
+      goto success;
+    err = errno;
+    close(sock);
   }
+
+  alarm(0);
+  sigaction(SIGALRM, &oALRM, 0);
+  sigaction(SIGINT, &oINT, 0);
+
   switch (err) {
-  case ECONNREFUSED: puts("status:tcp-refused");     return 0;
-  case ECONNRESET:   puts("status:tcp-reset");       return 0;
   case EHOSTUNREACH:
-  case ENETUNREACH:  puts("status:tcp-unreachable"); return 0;
+  case ENETUNREACH:  puts("status:tcp-unreachable"); break;
+  case ECONNREFUSED: puts("status:tcp-refused");     break;
+  case ECONNRESET:   puts("status:tcp-reset");       break;
+  case EINTR:        puts("status:timeout");         break;
   default:
     puts("status:tcp-error");
-    return 1;
+    printf("detail:%s\n", strerror(err));
   }
+  return -1;
+
+ success:
+  alarm(0);
+  sigaction(SIGALRM, &oALRM, 0);
+  sigaction(SIGINT, &oINT, 0);
+  return sock;
 }
 
 static int
-status_tls_error(CURLcode res, const char *errbuf)
+tls_handshake(gnutls_session_t session, int sock)
 {
-  puts("status:tls-error");
-  return 2;
+  gnutls_transport_set_int(session, sock);
+  gnutls_handshake_set_timeout(session, GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
+
+  int ret;
+  do {
+    ret = gnutls_handshake(session);
+  } while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+
+  if (ret >= 0) {
+    gnutls_bye(session, GNUTLS_SHUT_RDWR);
+    return 0;
+  }
+
+  printf("*** Handshake failed: %s\n", gnutls_strerror(ret));
+  if (ret == GNUTLS_E_FATAL_ALERT_RECEIVED ||
+      ret == GNUTLS_E_WARNING_ALERT_RECEIVED) {
+    printf("*** Alert detail: %s\n",
+           gnutls_alert_get_name(gnutls_alert_get(session)));
+
+  } else if (ret == GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR) {
+    int type;
+    unsigned status;
+    gnutls_datum_t out;
+
+    /* check certificate verification status */
+    type = gnutls_certificate_type_get(session);
+    status = gnutls_session_get_verify_cert_status(session);
+    ret = gnutls_certificate_verification_status_print(status, type, &out, 0);
+    if (ret < 0) {
+      printf("*** cannot report verification status: %s\n",
+             gnutls_strerror(ret));
+    } else {
+      printf("*** cert verify output: %s\n", out.data);
+      gnutls_free(out.data);
+    }
+  }
+  return -1;
 }
 
 static void
-report_status(CURL *curl, CURLcode res, const char *errbuf)
+setup_gnutls(gnutls_session_t *session,
+             gnutls_certificate_credentials_t *xcred,
+             const char *host)
 {
-  switch (res) {
-  case CURLE_OK:
-    puts("status:success\n");
-    return;
-
-  case CURLE_UNSUPPORTED_PROTOCOL:
-  case CURLE_URL_MALFORMAT:
-  case CURLE_LDAP_INVALID_URL:
-    puts("status:invalid-url");
-    detail_dump_errbuf(res, errbuf);
-    return;
-
-  case CURLE_COULDNT_RESOLVE_PROXY:
-  case CURLE_COULDNT_RESOLVE_HOST:
-  case CURLE_FTP_CANT_GET_HOST:
-    if (status_dns_error(res, errbuf))
-      detail_dump_errbuf(res, errbuf);
-    return;
-
-  case CURLE_COULDNT_CONNECT:
-  case CURLE_INTERFACE_FAILED:
-    if (status_tcp_error(curl, errbuf))
-      detail_dump_errbuf(res, errbuf);
-    return;
-
-  case CURLE_WEIRD_SERVER_REPLY:
-  case CURLE_REMOTE_ACCESS_DENIED:
-  case CURLE_FTP_ACCEPT_FAILED:
-  case CURLE_FTP_WEIRD_PASS_REPLY:
-  case CURLE_FTP_WEIRD_PASV_REPLY:
-  case CURLE_FTP_WEIRD_227_FORMAT:
-  case CURLE_HTTP2:
-  case CURLE_FTP_COULDNT_SET_TYPE:
-  case CURLE_PARTIAL_FILE:
-  case CURLE_FTP_COULDNT_RETR_FILE:
-  case CURLE_QUOTE_ERROR:
-  case CURLE_HTTP_RETURNED_ERROR:
-  case CURLE_UPLOAD_FAILED:
-  case CURLE_FTP_PORT_FAILED:
-  case CURLE_FTP_COULDNT_USE_REST:
-  case CURLE_RANGE_ERROR:
-  case CURLE_HTTP_POST_ERROR:
-  case CURLE_LDAP_CANNOT_BIND:
-  case CURLE_LDAP_SEARCH_FAILED:
-  case CURLE_TOO_MANY_REDIRECTS:
-  case CURLE_TELNET_OPTION_SYNTAX:
-  case CURLE_SEND_ERROR:
-  case CURLE_RECV_ERROR:
-  case CURLE_BAD_CONTENT_ENCODING:
-  case CURLE_USE_SSL_FAILED:
-  case CURLE_LOGIN_DENIED:
-  case CURLE_TFTP_NOTFOUND:
-  case CURLE_TFTP_PERM:
-  case CURLE_REMOTE_DISK_FULL:
-  case CURLE_TFTP_ILLEGAL:
-  case CURLE_TFTP_UNKNOWNID:
-  case CURLE_REMOTE_FILE_EXISTS:
-  case CURLE_TFTP_NOSUCHUSER:
-  case CURLE_REMOTE_FILE_NOT_FOUND:
-  case CURLE_SSH:
-  case CURLE_SSL_SHUTDOWN_FAILED:
-  case CURLE_HTTP2_STREAM:
-    puts("status:proto-error");
-    detail_dump_errbuf(res, errbuf);
-    return;
-
-  case CURLE_FTP_ACCEPT_TIMEOUT:
-  case CURLE_OPERATION_TIMEDOUT:
-    puts("status:timeout");
-    return;
-
-  case CURLE_SSL_CONNECT_ERROR:
-  case CURLE_PEER_FAILED_VERIFICATION:
-  case CURLE_SSL_CERTPROBLEM:
-  case CURLE_SSL_CIPHER:
-  case CURLE_SSL_CACERT:
-  case CURLE_SSL_ISSUER_ERROR:
-  case CURLE_SSL_PINNEDPUBKEYNOTMATCH:
-  case CURLE_SSL_INVALIDCERTSTATUS:
-    {
-      int x = status_tls_error(res, errbuf);
-      if (x >= 1)
-        detail_dump_errbuf(res, errbuf);
-      if (x >= 2)
-        cert_dump_chain(curl, errbuf);
-    }
-    return;
-
-  case CURLE_OBSOLETE29:
-  case CURLE_OBSOLETE32:
-  case CURLE_BAD_DOWNLOAD_RESUME:
-  case CURLE_FILE_COULDNT_READ_FILE:
-  case CURLE_OBSOLETE40:
-  case CURLE_FUNCTION_NOT_FOUND:
-  case CURLE_ABORTED_BY_CALLBACK:
-  case CURLE_BAD_FUNCTION_ARGUMENT:
-  case CURLE_OBSOLETE44:
-  case CURLE_OBSOLETE46:
-  case CURLE_UNKNOWN_OPTION:
-  case CURLE_OBSOLETE50:
-  case CURLE_GOT_NOTHING:
-  case CURLE_SSL_ENGINE_NOTFOUND:
-  case CURLE_SSL_ENGINE_SETFAILED:
-  case CURLE_OBSOLETE57:
-  case CURLE_FILESIZE_EXCEEDED:
-  case CURLE_SEND_FAIL_REWIND:
-  case CURLE_SSL_ENGINE_INITFAILED:
-  case CURLE_CONV_FAILED:
-  case CURLE_CONV_REQD:
-  case CURLE_SSL_CACERT_BADFILE:
-  case CURLE_AGAIN:
-  case CURLE_SSL_CRL_BADFILE:
-  case CURLE_FTP_PRET_FAILED:
-  case CURLE_RTSP_CSEQ_ERROR:
-  case CURLE_RTSP_SESSION_ERROR:
-  case CURLE_FTP_BAD_FILE_LIST:
-  case CURLE_CHUNK_FAILED:
-  case CURLE_NO_CONNECTION_AVAILABLE:
-  case CURLE_FAILED_INIT:
-  case CURLE_NOT_BUILT_IN:
-  case CURLE_OBSOLETE20:
-  case CURLE_WRITE_ERROR:
-  case CURLE_OBSOLETE24:
-  case CURLE_READ_ERROR:
-  case CURLE_OUT_OF_MEMORY:
-  case CURL_LAST:
-    fputs("error: curl_easy_perform: ", stderr);
-    dump_errbuf(res, errbuf, stderr);
+  if (!gnutls_check_version("3.4.6")) {
+    fputs("GnuTLS library too old (>=3.4.6 required)\n", stderr);
     exit(1);
   }
+
+  const char *what;
+  int res;
+  const char *errptr = 0;
+#define XGT(fn, args) do {                              \
+    what = #fn;                                         \
+    if ((res = gnutls_##fn args) < 0) goto fail;        \
+  } while (0)
+
+  XGT(global_init, ());
+  XGT(certificate_allocate_credentials, (xcred));
+  XGT(certificate_set_x509_system_trust, (*xcred));
+
+  XGT(init, (session, GNUTLS_CLIENT));
+  XGT(server_name_set, (*session, GNUTLS_NAME_DNS, host, strlen(host)));
+  XGT(credentials_set, (*session, GNUTLS_CRD_CERTIFICATE, *xcred));
+  gnutls_session_set_verify_cert(*session, host, 0); /* can't fail */
+
+  /* This priority string matches the out-of-the-box ClientHello
+     generated by Firefox 52.0.2 (ESR) as closely as possible.  */
+  XGT(priority_set_direct,
+      (*session,
+       "NONE:%COMPAT"
+       ":+VERS-TLS1.2:+VERS-TLS1.1:+VERS-TLS1.0:+CTYPE-X.509"
+       ":+AES-128-GCM:+CHACHA20-POLY1305:+AES-256-GCM:+AES-256-CBC:+AES-128-CBC:+3DES-CBC"
+       ":+AEAD:+SHA1"
+       ":+ECDHE-ECDSA:+ECDHE-RSA:+DHE-RSA:+RSA"
+       ":+COMP-NULL"
+       ":+CURVE-X25519:+CURVE-SECP256R1:+CURVE-SECP384R1:+CURVE-SECP521R1"
+       ":+SIGN-ECDSA-SHA256:+SIGN-ECDSA-SHA384:+SIGN-ECDSA-SHA512"
+     //":+SIGN-RSA-PSS-SHA256:+SIGN-RSA-PSS_SHA384:+SIGN-RSA-PSS-SHA512" not supported by gnutls 3.5
+       ":+SIGN-RSA-SHA256:+SIGN-RSA-SHA384:+SIGN-RSA-SHA512:+SIGN-ECDSA-SHA1:+SIGN-RSA-SHA1"
+       , &errptr));
+
+  /* Similarly, 1023-bit DH primes are the minimum in Firefox 52.0.2.
+     We don't use SRP in this program so we don't need to set that.  */
+  gnutls_dh_set_prime_bits(*session, 1023);
+
+  /* Match ALPN extension sent by Firefox as well.  */
+  static unsigned char pr1[3] = "h2";
+  static unsigned char pr2[9] = "http/1.1";
+  static gnutls_datum_t protocols[] = { { pr1, 2 }, { pr2, 8 } };
+  gnutls_alpn_set_protocols(*session, protocols, 2, 0);
+
+  return;
+
+ fail:
+  fprintf(stderr, "%s: %s\n", what, gnutls_strerror(res));
+  if (errptr)
+    fprintf(stderr, "%s: at %.20s\n", what, errptr);
+  exit(1);
+}
+
+static void
+teardown_gnutls(gnutls_session_t session,
+                gnutls_certificate_credentials_t xcred)
+{
+  gnutls_deinit(session);
+  gnutls_certificate_free_credentials(xcred);
+  gnutls_global_deinit();
+}
+
+static bool
+tls_port_p(const char *port)
+{
+  return !strcmp(port, "443");
 }
 
 int
 main(int argc, char **argv)
 {
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s URL\n", argv[0]);
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s HOST PORT\n", argv[0]);
     return 2;
   }
 
-  const char *url = argv[1];
+  const char *host = argv[1];
+  const char *port = argv[2];
 
-  curl_global_init(CURL_GLOBAL_ALL|CURL_GLOBAL_ACK_EINTR);
-  CURL *curl = curl_easy_init();
-  if (!curl) {
-    fputs("error: curl_easy_init failed\n", stderr);
-    return 1;
-  }
+  bool do_tls = tls_port_p(port);
 
-  CURLcode res;
-  char errbuf[CURL_ERROR_SIZE];
-  const char *what;
-  errbuf[0] = '\0';
+  gnutls_certificate_credentials_t xcred;
+  gnutls_session_t session;
+  if (do_tls)
+    setup_gnutls(&session, &xcred, host);
 
-#define curl_xsetopt(h, k, v) do {                               \
-    what = #k;                                                   \
-    if ((res = curl_easy_setopt(h, CURLOPT_##k, v))) goto error; \
-  } while (0)
+  struct addrinfo *addrlist = dns_lookup(host, port);
+  if (!addrlist)
+    return 0;
 
-  curl_xsetopt(curl, ERRORBUFFER, errbuf);
-  curl_xsetopt(curl, URL, url);
-  curl_xsetopt(curl, CONNECT_ONLY, 1L);
-  curl_xsetopt(curl, CERTINFO, 1L);
+  int sock = tcp_connect(addrlist);
+  freeaddrinfo(addrlist);
+  if (sock < 0)
+    return 0;
 
-  res = curl_easy_perform(curl);
-  report_status(curl, res, errbuf);
-  curl_easy_cleanup(curl);
+  if (!do_tls || !tls_handshake(session, sock))
+    puts("status:success");
+
+  close(sock);
+
+  if (do_tls)
+    teardown_gnutls(session, xcred);
+
   return 0;
-
- error:
-  fprintf(stderr, "error: curl_easy_setopt(%s): ", what);
-  dump_errbuf(res, errbuf, stderr);
-  return 1;
 }
